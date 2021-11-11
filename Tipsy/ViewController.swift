a1990a4515967590df9910e218406d2831e33ade
Raeein Bagheri <raeeinbagheri80@gmail.com>
2021-11-11 18:03:03 -0500

    finish first ViewController

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmout: UITextField!
+    @IBOutlet weak var zeroPercent: UIButton!
+    @IBOutlet weak var twentyPercent: UIButton!
+    @IBOutlet weak var tenPercent: UIButton!
+    @IBOutlet weak var splitAmount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tipAmount(_ sender: UIButton) {
+        zeroPercent.isSelected = false
+        tenPercent.isSelected = false
+        twentyPercent.isSelected = false
+        sender.isSelected = true
+    }
+    @IBAction func splitAmountChanged(_ sender: UIStepper) {
+        splitAmount.text = String(format: "%.0f", sender.value)
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        print("Hello")
+    }
 }
 
